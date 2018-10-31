ade8af09c28eb8c5cbc12dff9d0d602077783ebc
Talisa Erazo <talisaerazo@gmail.com>
2018-10-31 19:56:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 class Dog
-  def initialize(name)
-    @name = name
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
+
   def name
-    @name
-  end
-  def name=(new_name)
-    @name = new_name
+    @this_dogs_name
   end
 end
